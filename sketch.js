feb2af7df29d732ea0bdb3ecc50842e1833847d6
Ria Jayakar <71960598+RiaJayakar13@users.noreply.github.com>
2020-12-27 12:54:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var ball;
+var position ; 
+var database;
+
+function setup(){
+   database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+var ballPosition=database.ref('Ball/Position');
+ballPosition.on("value",readPosition,showError);
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        WritePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      WritePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        WritePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        WritePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function WritePosition(x,y){
+   
+    database.ref('Ball/Position').set({
+        'x' : position.x + x,
+       'y' : position.y + y
+
+    })
+}
+   function readPosition(data){
+
+position=data.val();
+ball.x=position.x;
+ball.y=position.y;
+
+   }
+
+function showError(){
+
+console.log("error");
+
+}
